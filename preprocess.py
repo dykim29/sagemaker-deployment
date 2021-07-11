ba520db332dd70ee86e1537b5a5d3e9f27b5a33d
DoYoung <doyoung_kim@hotmail.com>
2021-07-11 15:25:31 +0100

    [feat]preprocess data

---
+++ preprocess.py
@@ -0,0 +1,124 @@
+import os
+import glob
+from multiprocessing import Pool
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import *
+from sklearn.utils import shuffle
+import pickle
+
+cache_dir = os.path.join("../cache", "sentiment_analysis")  # where to store cache files
+os.makedirs(cache_dir, exist_ok=True)  # ensure cache directory exists
+
+import re
+from bs4 import BeautifulSoup
+
+def read_imdb_data(data_dir='../data/aclImdb'):
+    data = {}
+    labels = {}
+    
+    for data_type in ['train', 'test']:
+        data[data_type] = {}
+        labels[data_type] = {}
+        
+        for sentiment in ['pos', 'neg']:
+            data[data_type][sentiment] = []
+            labels[data_type][sentiment] = []
+            
+            path = os.path.join(data_dir, data_type, sentiment, '*.txt')
+            files = glob.glob(path)
+            
+            for f in files:
+                with open(f) as review:
+                    data[data_type][sentiment].append(review.read())
+                    # Here we represent a positive review by '1' and a negative review by '0'
+                    labels[data_type][sentiment].append(1 if sentiment == 'pos' else 0)
+                    
+            assert len(data[data_type][sentiment]) == len(labels[data_type][sentiment]), \
+                    "{}/{} data size does not match labels size".format(data_type, sentiment)
+                
+    return data, labels
+
+
+def prepare_imdb_data(data, labels):
+    """Prepare training and test sets from IMDb movie reviews."""
+
+    #Combine positive and negative reviews and labels
+    data_train = data['train']['pos'] + data['train']['neg']
+    data_test = data['test']['pos'] + data['test']['neg']
+    labels_train = labels['train']['pos'] + labels['train']['neg']
+    labels_test = labels['test']['pos'] + labels['test']['neg']
+
+    #Shuffle reviews and corresponding labels within training and test sets
+    data_train, labels_train = shuffle(data_train, labels_train)
+    data_test, labels_test = shuffle(data_test, labels_test)
+
+    # Return a unified training data, test data, training labels, test labets
+    return data_train, data_test, labels_train, labels_test
+
+
+def review_to_words(review):
+    nltk.download("stopwords", quiet=True)
+    stemmer = PorterStemmer()
+
+    text = BeautifulSoup(review, "html.parser").get_text() # Remove HTML tags
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower()) # Convert to lower case
+    words = text.split() # Split string into words
+    words = [w for w in words if w not in stopwords.words("english")] # Remove stopwords
+    words = [PorterStemmer().stem(w) for w in words] # stem
+
+    return words
+
+
+def preprocess_data_with_multiprocessing(data_train, data_test, labels_train, labels_test,
+                    cache_dir=cache_dir, cache_file="preprocessed_data.pkl"):
+    """Convert each review to words; read from cache if available."""
+
+    # If cache_file is not None, try to read from it first
+    cache_data = None
+    if cache_file is not None:
+        try:
+            with open(os.path.join(cache_dir, cache_file), "rb") as f:
+                cache_data = pickle.load(f)
+            print("Read preprocessed data from cache file:", cache_file)
+        except:
+            pass  # unable to read from cache, but that's okay
+
+    # If cache is missing, then do the heavy lifting
+    if cache_data is None:
+        # Preprocess training and test data to obtain words for each review
+        # words_train = list(map(review_to_words, data_train))
+        # words_test = list(map(review_to_words, data_test))
+        with Pool() as pool:
+            words_train = pool.map(review_to_words, data_train)
+        print('done processing train words')
+        with Pool() as pool:
+            words_test = pool.map(review_to_words, data_test)
+        print('done processing test words')
+        # Write to cache file for future runs
+        if cache_file is not None:
+            cache_data = dict(words_train=words_train, words_test=words_test,
+                              labels_train=labels_train, labels_test=labels_test)
+            with open(os.path.join(cache_dir, cache_file), "wb") as f:
+                pickle.dump(cache_data, f)
+            print("Wrote preprocessed data to cache file:", cache_file)
+    else:
+        # Unpack data loaded from cache file
+        words_train, words_test, labels_train, labels_test = (cache_data['words_train'],
+                                                              cache_data['words_test'], cache_data['labels_train'],
+                                                              cache_data['labels_test'])
+
+    return words_train, words_test, labels_train, labels_test
+
+
+if __name__ == '__main__':
+    data, labels = read_imdb_data()
+    print("IMDB reviews: train = {} pos / {} neg, test = {} pos / {} neg".format(
+                len(data['train']['pos']), len(data['train']['neg']),
+                len(data['test']['pos']), len(data['test']['neg'])))
+    train_X, test_X, train_y, test_y = prepare_imdb_data(data, labels)
+    print("IMDb reviews (combined): train = {}, test = {}".format(len(train_X), len(test_X)))
+
+    # Preprocess data
+    train_X, test_X, train_y, test_y = preprocess_data_with_multiprocessing(train_X, test_X, train_y, test_y)
